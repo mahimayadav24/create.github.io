daf8a422c9d68759b6657be70e52bd9f4e4f67a8
mahimayadav24 <88049838+mahimayadav24@users.noreply.github.com>
2021-09-21 19:43:40 +0530

    insert

---
+++ JS/donutchart.js
@@ -0,0 +1,80 @@
+
+var options = {
+    series: [30, 30, 30],
+    chart: {
+        width: 380,
+        type: 'donut',
+
+    },
+    dataLabels: {
+        enabled: false
+    },
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            chart: {
+                width: 200
+            },
+            legend: {
+                show: false
+            }
+        }
+    }],
+
+    legend: {
+        position: 'right',
+        offsetY: 0,
+        height: 230,
+    },
+    fill: {
+        colors: ['#FFF736', '#FEAD34', '#24CDE2']
+    },
+};
+
+
+
+
+
+var chart = new ApexCharts(document.querySelector("#charts"), options);
+chart.render();
+
+
+function appendData() {
+    var arr = chart.w.globals.series.slice()
+    arr.push(Math.floor(Math.random() * (100 - 1 + 1)) + 1)
+    return arr;
+}
+
+function removeData() {
+    var arr = chart.w.globals.series.slice()
+    arr.pop()
+    return arr;
+    c
+}
+
+function randomize() {
+    return chart.w.globals.series.map(function () {
+        return Math.floor(Math.random() * (100 - 1 + 1)) + 1
+    })
+}
+
+function reset() {
+    return options.series
+}
+
+document.querySelector("#randomize").addEventListener("click", function () {
+    chart.updateSeries(randomize())
+})
+
+document.querySelector("#add").addEventListener("click", function () {
+    chart.updateSeries(appendData())
+})
+
+document.querySelector("#remove").addEventListener("click", function () {
+    chart.updateSeries(removeData())
+})
+
+document.querySelector("#reset").addEventListener("click", function () {
+    chart.updateSeries(reset())
+})
+
